7f39235cf7d331c9e8f3c5bde1207d3350c854ca
Angelo <aquaaticnaotrava@gmail.com>
2023-04-11 15:17:53 -0300

    Alterados códigos de status incorretos de 401 para 403

--- src/middlewares/autorizacao.middleware.ts
+++ src/middlewares/autorizacao.middleware.ts
@@ -7,7 +7,7 @@ import { usuariosEnum } from "../types/enums";
 export const authParticipante = async (req: Request, res: Response, next: NextFunction) => {
     const participante = await ParticipanteModel.findById(req.userId);
     if(!participante)
-        return res.status(401).json({ msg: "Não autorizado", erro: true });
+        return res.status(403).json({ msg: "Não autorizado", erro: true });
     
     req.participante = participante;
     next();
@@ -16,7 +16,7 @@ export const authParticipante = async (req: Request, res: Response, next: NextFu
 export const authUsuario = async (req: Request, res: Response, next: NextFunction) => {
     const usuario = await UsuarioModel.findById(req.userId);
     if(!usuario)
-        return res.status(401).json({ msg: "Não autorizado", erro: true });
+        return res.status(403).json({ msg: "Não autorizado", erro: true });
 
     req.usuario = usuario;
     next();
@@ -25,7 +25,7 @@ export const authUsuario = async (req: Request, res: Response, next: NextFunctio
 export const authInstituicao = async (req: Request, res: Response, next: NextFunction) => {
     const instituicao = await InstituicaoModel.findById(req.userId);
     if(!instituicao)
-        return res.status(401).json({ msg: "Não autorizado", erro: true });
+        return res.status(403).json({ msg: "Não autorizado", erro: true });
     
     req.instituicao = instituicao;
     next();
@@ -34,11 +34,11 @@ export const authInstituicao = async (req: Request, res: Response, next: NextFun
 export const authOperador = async (req: Request, res: Response, next: NextFunction) => {
     const operador = await OperadorModel.findById(req.userId);
     if(!operador)
-        return res.status(401).json({ msg: "Não autorizado", erro: true });
+        return res.status(403).json({ msg: "Não autorizado", erro: true });
     else if(!operador.confirmado)
-        return res.status(401).json({ msg: "Não autorizado: sua conta ainda não foi ativada", erro: true });
+        return res.status(403).json({ msg: "Não autorizado: sua conta ainda não foi ativada", erro: true });
     else if(!operador.ativo)
-        return res.status(401).json({ msg: "Não autorizado: Sua conta está bloqueada, contate seu gestor", erro: true });
+        return res.status(403).json({ msg: "Não autorizado: Sua conta está bloqueada, contate seu gestor", erro: true });
 
     req.operador = operador;
     next();
@@ -48,5 +48,5 @@ export const authInstitucional = async (req: Request, res: Response, next: NextF
     if(req.usuario!.tipo === usuariosEnum.Instituicao || req.usuario!.tipo === usuariosEnum.Operador)
         next();
     else
-        res.status(401).json({ msg: "Não autorizado: você não faz parte de uma instituição", erro: true });
+        res.status(403).json({ msg: "Não autorizado: você não faz parte de uma instituição", erro: true });
 };
